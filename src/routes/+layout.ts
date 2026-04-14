067cb9435ac319cc0f4dda01d2cf7ef911a7c89b
dzface <dzface031@gmail.com>
2026-04-14 21:50:16 +0900

    git init

---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+export const prerender = true // HTML 파일로 미리 만들어져 서버없이 동작
+export const trailingSlash = 'always' // URL 끝에 항상 / 붙임~
---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+	},
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: '404.html',
+			precompress: true,
+			strict: true
+		})
+	},
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) =>
+			filename.includes('node_modules') ? undefined : { runes: true }
+	}
+};
+
+export default config;
